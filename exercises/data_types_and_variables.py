34644363d373b1632ca1b65b5704bf8cbf471ca1
David Schneemann <david.schneemann1@gmail.com>
2022-05-26 14:56:03 -0700

    add .py files; exercises

---
+++ exercises/data_types_and_variables.py
@@ -0,0 +1,62 @@
+### Movies for the kids
+##You have rented some movies for your kids: The little mermaid (for 3 days), Brother Bear 
+#(for 5 days, they love it), and Hercules (1 day, you don't know yet if 
+# they're going to like it). If price for a movie per day is 3 dollars, how much will you have to pay?
+
+little_mermaid_days = 3
+brother_bear_days = 5
+hercules_days = 1
+cost_per_day = 3
+total = little_mermaid_days * cost_per_day + hercules_days * cost_per_day + hercules_days * cost_per_day
+total
+
+
+##Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you 
+# a different rate per hour. Google pays 400 dollars per hour, Amazon 380, and Facebook 350. 
+# How much will you receive in payment for this week? You worked 10 hours for Facebook, 
+# 6 hours for Google and 4 hours for Amazon.
+
+hourly_rate_google = 400
+
+hours_worked_google = 10
+
+
+# make calculations using variables
+total_pay_google = hourly_rate_google * hours_worked_google
+total_pay_amazon = 
+
+total_pay = total_pay_google + 
+
+total_pay
+
+
+
+## A student can be enrolled to a class only if the class is not full and the class schedule does not 
+# conflict with her current schedule.
+
+
+
+
+
+
+
+
+
+### Continue working in your data_types_and_variables.py file. 
+# Use the following code to follow the instructions below:
+
+username = 'codeup'
+password = 'notastrongpassword'
+
+# Create a variable that holds a boolean value for each of the following conditions:
+
+
+password >= len(5)
+
+password <= len(20)
+
+password == username
+
+
+
+
---
+++ exercises/list_comprehensions.py
@@ -0,0 +1,20 @@
+### List Comprehensions
+
+fruits = ['mango', 'kiwi', 'strawberry', 'guava', 'pineapple', 'mandarin orange']
+
+numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 17, 19, 23, 256, -8, -4, -2, 5, -9]
+
+# Ex 1
+fruits
+[fruit for fruit in fruits(6)]
+uppercased_fruits = fruits.upper()
+
+
+# Ex 2
+
+capitalized_fruits = [fruit.capitalize() for fruit in fruits]
+
+
+# Ex 3
+
+fruits_with_more_than_two_vowels = [fruit for fruit in fruits if ]
